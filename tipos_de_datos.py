f12450d92d12d9d70edf6e60648579dc7d9848a0
Dcas45 <145266134+Dcas45@users.noreply.github.com>
2024-08-01 19:41:37 -0500

    Add files via upload

---
+++ tipos_de_datos.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+#arr=np.array([1,2,3,4], dtype='float64')# Definiendo un array con una lista
+#print(arr)
+
+#arr.dtype
+#print(arr.dtype)
+#arr=np.array([1,2,3,4] )
+#print(arr)
+#arr=np.array([0,1,2,3,4] )
+#arr=arr.astype(np.bool_ )
+#print(arr)
+#arr=np.array([0,1,2,3,4] )
+#arr=arr.astype(np.str_ )
+#print(arr)
+arr=np.array(['0', '1', '2', '3', '4'] )
+arr=arr.astype(np.int8) # Cambia tipo de dato
+print(arr)
